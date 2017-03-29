8830d50b8c431ad682391a70d845995198af0fc7
Gena <gena@e64.net>
2017-03-29 13:17:26 +0200

    exo1

--- conditions/if-compose-avance.php
+++ conditions/if-compose-avance.php
@@ -14,9 +14,9 @@ echo "<br>";
 echo "Mon chat est vivant, de couleur blanc et a les yeux Vert";
 echo "<br>";
 if($animal == "Chat" && $vivant && $couleur = "Blanc" && $yeux = "Vert"){
-  echo "Oui";
+	echo "Oui";
 }else{
-  echo "Non";
+	echo "Non";
 }
 echo "<br><br>";
 
@@ -30,17 +30,17 @@ echo "1.Est-ce que c'est un animal fait l'affaire ? ";
 echo "<br>";
 echo "Je veux un chien ou chat couleur marron avec des yeux verts ";
 echo "<br>";
-//n'hésitez pas à changer les valeurs pour tester si ça fonctionne
+
 $animal = "Chat";
 $vivant = true;
 $couleur = "Marron";
 $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
-if( 0 ){
-  echo 'Oui';
+if($animal = "Chat ou chien" && $vivant && $couleur ='Marron' && $yeux ='Vert'){
+	echo 'Oui';
 }else {
-  echo 'Non';
+	echo 'Non';
 }
 echo "<br><br>";
 
@@ -58,9 +58,9 @@ $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
 if( 0 ){
-  echo 'Oui';
+	echo 'Oui';
 }else {
-  echo 'Non';
+	echo 'Non';
 }
 echo "<br><br>";
 
@@ -78,9 +78,9 @@ $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
 if( 0 ){
-  echo 'Oui';
+	echo 'Oui';
 }else {
-  echo 'Non';
+	echo 'Non';
 }
 echo "<br><br>";
 //----------------------------------------
